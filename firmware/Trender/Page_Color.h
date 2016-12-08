c4901a26a7e2274518367256f19edf6ac08f3dfc
jerome-labidurie <jerome@labidurie.fr>
2016-12-08 18:58:16 +0100

    Add GPLv3 licence information

--- firmware/Trender/Page_Color.h
+++ firmware/Trender/Page_Color.h
@@ -1,10 +1,30 @@
+/**
+ *  This file is part of Trender.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Copyright 2016 Jérôme Labidurie jerome@labidurie.fr
+ *  Copyright 2016 Cédric Bernard cedric.bernard@galilabs.com
+ *  Copyright 2016 Julien Jacques julien.jacques@galilabs.com
+ */
 
 
 const char PAGE_Color[] = R"=====(
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link rel="stylesheet" href="style.css" type="text/css" />
-<script src="microajax.js"></script> 
+<script src="microajax.js"></script>
 <a href="/"  class="btn btn--s"><</a>&nbsp;&nbsp;<strong>Color</strong>
 <hr>
 <form action="" method="post">
@@ -37,7 +57,7 @@ void sendColorData () {
   String values ="";
   values += "rainbow|" +  (String) (showRainbow ? "checked" : "") + "|chk\n";
   server.send ( 200, "text/plain", values);
-  Serial.println(__FUNCTION__); 
+  Serial.println(__FUNCTION__);
 }
 
 void processColor ()
@@ -56,6 +76,6 @@ void processColor ()
   }
 
   server.send ( 200, "text/html", PAGE_Color);
-  Serial.println(__FUNCTION__); 
+  Serial.println(__FUNCTION__);
 
 }
--- firmware/Trender/Page_Information.h
+++ firmware/Trender/Page_Information.h
@@ -1,3 +1,24 @@
+/**
+ *  This file is part of Trender.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Copyright 2016 Jérôme Labidurie jerome@labidurie.fr
+ *  Copyright 2016 Cédric Bernard cedric.bernard@galilabs.com
+ *  Copyright 2016 Julien Jacques julien.jacques@galilabs.com
+ */
+
 #ifndef PAGE_INFOMATION_H
 #define PAGE_INFOMATION_H
 
@@ -9,7 +30,7 @@ const char PAGE_Information[]  = R"=====(
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link rel="stylesheet" href="style.css" type="text/css" />
-<script src="microajax.js"></script> 
+<script src="microajax.js"></script>
 <a href="/"  class="btn btn--s"><</a>&nbsp;&nbsp;<strong>Network Information</strong>
 <hr>
 <table border="0"  cellspacing="0" cellpadding="3" style="width:310px" >
@@ -28,9 +49,9 @@ function GetState()
 
 window.onload = function ()
 {
-  load("style.css","css", function() 
+  load("style.css","css", function()
   {
-    load("microajax.js","js", function() 
+    load("microajax.js","js", function()
     {
         GetState();
     });
@@ -55,7 +76,7 @@ String GetMacAddress()
 
 //
 // FILL WITH INFOMATION
-// 
+//
 
 void send_information_values_html ()
 {
@@ -66,7 +87,7 @@ void send_information_values_html ()
   values += "x_ip|" +  (String) WiFi.softAPIP()[0] + "." +  (String) WiFi.softAPIP()[1] + "." +  (String) WiFi.softAPIP()[2] + "." + (String) WiFi.softAPIP()[3] +  "|div\n";
   values += "x_mac|" + GetMacAddress() +  "|div\n";
   server.send ( 200, "text/plain", values);
-  Serial.println(__FUNCTION__); 
+  Serial.println(__FUNCTION__);
 
 }
 
